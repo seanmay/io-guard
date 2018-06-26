d7325bb7649fba8a537d05ae83ceb0998a001eaf
Sean May <sean.j.may@gmail.com>
2018-06-26 10:54:26 -0400

    Adding interface layout and initial types; full of holes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-interface TypeGuard<T> {
+export interface TypeGuard<T> {
   (x: ValueMap<T>): x is T;
 }
 
-type ValidatorMap<T> = { [key in keyof T]: TypeGuard<T[key]> };
+export interface TypedTest<T> {
+  (x: T): any;
+}
 
-type ValueMap<T> = { [key in keyof T]: T[key] };
+export type ValidatorMap<T> = { [key in keyof T]: TypeGuard<T[key]> };
+
+export type ValueMap<T> = { [key in keyof T]: T[key] };
 
 const Guard = <T>(validators: ValidatorMap<T>): TypeGuard<T> => (
   values: ValueMap<T>
@@ -12,11 +16,19 @@ const Guard = <T>(validators: ValidatorMap<T>): TypeGuard<T> => (
   for (const key in validators) {
     const test = validators[key];
     const value = values[key];
-    if (value == null || !test(value)) {
-      return false;
-    } else { continue; }
+    if (value == null || !test(value)) return false;
+    else continue;
   }
   return true;
 };
 
 export { Guard };
+
+export {
+  isNumber,
+  isString,
+  isBoolean,
+  isFunction,
+  isArray
+} from "./guards/index";
+export { compose, and, or, optional } from "./operators/index";
