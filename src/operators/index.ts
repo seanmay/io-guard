9801cbe7f3b9489748329372252fc20131cc491e
Sean May <sean.j.may@gmail.com>
2018-07-09 00:44:28 -0400

    Quick refactor of operators to pull out null/undef checks.

--- src/operators/index.ts
+++ src/operators/index.ts
@@ -1,19 +1,25 @@
 import { TypedTest, Erratic } from "../types";
 
+const isNull = <T>(x: Erratic<T>): x is null => x === null;
+const isUndefined = <T>(x: Erratic<T>): x is undefined => x === undefined;
+
+const isErratic = <T>(x: Erratic<T>): x is null | undefined =>
+  isNull(x) || isUndefined(x);
+
 const and = <T>(...tests: TypedTest<T>[]) => (x: Erratic<T>): x is T =>
-  x == null ? false : tests.every(test => test(x));
+  isErratic(x) ? false : tests.every(test => test(x));
 
 const or = <T>(...tests: TypedTest<T>[]) => (x: Erratic<T>): x is T =>
-  x == null ? false : tests.some(test => test(x));
+  isErratic(x) ? false : tests.some(test => test(x));
 
 const optional = <T>(test: TypedTest<T>) => (x: Erratic<T>): x is T =>
-  x === null ? false : x === undefined ? true : test(x);
+  isNull(x) ? false : isUndefined(x) ? true : test(x);
 
 const nullable = <T>(test: TypedTest<T>) => (x: Erratic<T>): x is T =>
-  x === null ? true : x === undefined ? false : test(x);
+  isNull(x) ? true : isUndefined(x) ? false : test(x);
 
 const erratic = <T>(test: TypedTest<T>) => (x: Erratic<T>): x is T =>
-  x === null || x === undefined ? true : test(x);
+  isErratic(x) ? true : test(x);
 
 const compose = and;
 
