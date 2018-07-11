49ca1f009c812ce57e01aa66704d661e74de1a96
Sean May <sean.j.may@gmail.com>
2018-07-11 03:08:40 -0400

    Adds existence chek to Guard and array check to GuardEach. Fixes #4 (#6)

--- src/guards.test.ts
+++ src/guards.test.ts
@@ -1,4 +1,6 @@
 import { Guard, GuardEach } from "./index";
+import { and } from "./operators/index";
+import { isString, isNumber } from "./guards/index";
 
 describe("Guard", () => {
   it("takes an object of keys and validators, validates input with the same keys, and passes if every validator passes (short-ciruit on fail)", () => {
@@ -27,6 +29,63 @@ describe("Guard", () => {
     // @ts-ignore
     expect(isPerson({ name: "tribble" })).toBe(false);
   });
+
+  it("fails gracefully, if passed null or undefined", () => {
+    const holmes: User = {
+      name: "Sherlock Holmes",
+      address: {
+        street: "221B Baker St",
+        city: "London",
+        geo: { lat: "51.520664584", long: "-0.15499938" }
+      }
+    };
+
+    // @ts-ignore
+    const dent: User = {
+      name: "Arthur Dent",
+      address: null
+    };
+
+    const lte = (max: number) => (x: string | number) => +x <= max;
+    const gte = (min: number) => (x: string | number) => +x >= min;
+
+    const isNumberAsString = (x: string) =>
+      typeof +x === "number" && !isNaN(+x);
+    const isValidLat = and(isNumberAsString, gte(-90), lte(90));
+    const isValidLong = and(isNumberAsString, gte(-180), lte(180));
+
+    const isValidAddress = Guard<Address>({
+      street: isString,
+      city: isString,
+      geo: Guard({ lat: isValidLat, long: isValidLong })
+    });
+
+    const isValidUser = Guard<User>({
+      name: isString,
+      address: isValidAddress
+    });
+
+    interface User {
+      name: string;
+      address: Address;
+    }
+
+    interface Address {
+      street: string;
+      city: string;
+      geo: Geo;
+    }
+
+    interface Geo {
+      lat: string;
+      long: string;
+    }
+
+    // @ts-ignore
+    expect(isValidUser(null)).toBe(false);
+    expect(isValidUser(holmes)).toBe(true);
+    expect(isValidUser(dent)).toBe(false);
+  });
 });
 
 describe("GuardEach", () => {
@@ -54,5 +113,18 @@ describe("GuardEach", () => {
 
     expect(isCrew(crew1)).toBe(true);
     expect(isCrew(crew2)).toBe(false);
+  });
+
+  it("returns false if given a value which is not an array", () => {
+    const allRight = GuardEach((x: boolean): x is true => x === true);
+
+    // @ts-ignore
+    expect(allRight({})).toBe(false);
+    // @ts-ignore
+    expect(allRight(false)).toBe(false);
+    // @ts-ignore
+    expect(allRight([false])).toBe(false);
+    expect(allRight([])).toBe(true);
+    expect(allRight([true])).toBe(true);
   });
 });
